6be3cfeabba7f54face92d478f9d0ecb1156761f
rd2020-alt <84294849+rd2020-alt@users.noreply.github.com>
2023-12-11 07:26:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("unirseBtn").addEventListener("click", function() {
+     
+    window.location.href = "login.html";
+  });
+});
